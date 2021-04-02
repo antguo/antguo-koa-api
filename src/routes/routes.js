d86dd3783b53614a0e49a1c903d432b0cec06251
guowenjun <973379770@qq.com>
2021-04-02 16:03:25 +0800

    add koa-helmet koa-json koa-router koa-static

---
+++ src/routes/routes.js
@@ -0,0 +1,10 @@
+// 合并routes操作
+const combineRoutes = require('koa-combine-routers')
+
+const aroutes = require('./aRouter')
+const broutes = require('./bRouter')
+
+module.exports = combineRoutes(
+  aroutes,
+  broutes
+)
