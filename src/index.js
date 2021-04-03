25d9fa05415aeab2cd47d7ec657f55daa10feb6e
guowenjun <973379770@qq.com>
2021-04-03 20:57:59 +0800

    添加es6语法支持，添加webpack打包配置

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-const Koa = require('koa')
-const path = require('path')
+import Koa from 'koa'
+import path from 'path'
+import helmet from 'koa-helmet'
+import statics from 'koa-static'
+import router from './routes/routes'
+
 const app = new Koa()
-const helmet = require('koa-helmet')
-const statics = require('koa-static')
-const router = require('./routes/routes')
 
 app.use(helmet())
 app.use(statics(path.join(__dirname,'../public')))
